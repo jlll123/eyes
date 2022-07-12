9a1419779ddfa591d8445e3636927597188c5fd9
jlll123 <96296509+jlll123@users.noreply.github.com>
2022-07-12 16:02:44 +0800

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -1,41 +1,42 @@
-"use strict";
-let capture;
-
+let eyes = []
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	capture = createCapture(VIDEO);
-	capture.hide();
-	capture.size(windowWidth, windowHeight);
-}
-
-
-function draw() {
-	let mainColor = '#9e0339';
-	let bgColor = '#ffee03';
-	background(bgColor);
-	noStroke();
-	fill(mainColor);
-
-	if (capture.width > 0) {
-		let img = capture.get(0, 0, capture.width, capture.height);
-		img.loadPixels();
-
-		const step = 15;
-		for (var y = step; y < img.height; y += step) {
-			for (var x = step; x < img.width; x += step) {
-				const darkness = getPixelDarknessAtPosition(img, x, y);
-				const radius = 20 * darkness;
-				let sX = x * width / img.width;
-				let sY = y * height / img.height;
-				circle(sX, sY, radius);
-			}
-		}
+	background(100);
+	for(var i=0;i<100;i++){
+		eyes.push({
+			x: random(width),
+			y: random(height),
+			sc: random()
+		})
 	}
 }
 
-//ignore this, initially
-function getPixelDarknessAtPosition(img, x, y) {
-	const mirroring = true;
-	var i = y * img.width + (mirroring ? (img.width - x - 1) : x);
-	return (255 - img.pixels[i * 4]) / 255;
+function drawEye(x,y,sc){
+	push()
+		translate(x,y)
+		scale(sc)
+		fill(255)
+		ellipse(0,0,200)
+		fill(0)
+		let ang = atan2(mouseY-y,
+										mouseX-x)
+		rotate(ang)
+		// ellipse(0,0,50,30)
+		ellipse(50,0,100)
+	pop()
+}
+
+function draw() {
+	background(0)
+	// for(var o=0;o<height;o+=100){a
+	// 	for(var i=0;i<width;i+=100){
+	for(var i=0;i<eyes.length;i++){
+		let eye =eyes[i]
+		drawEye(eye.x,eye.y,eye.sc)
+	}
+		// }
+	// }
+	
+	
+	// ellipse(mouseX, mouseY, 20, 20);
 }
